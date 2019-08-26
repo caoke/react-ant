208b99bcdac585ac319ce2c287bcc87ce08ce6f8
kecao <coco.cao@sf-express.com>
2019-08-26 18:36:57 +0800

    新增工程

---
+++ build/precache-manifest.27e1c02fea314d667115ac15e6ad385f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4d14b12ade865adc437dd980a6859c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f8391cc142cb220fa38",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5ea31f1f30a7ffdf207b",
+    "url": "/static/js/2.53b63a6f.chunk.js"
+  },
+  {
+    "revision": "0f8391cc142cb220fa38",
+    "url": "/static/js/main.a4229741.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
